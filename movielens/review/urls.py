e5c1a2a8bba43558ec0c3c99257864ae49461064
Kathleen Rauh <krauh2010@gmail.com>
2015-10-08 10:04:32 -0400

    Working on templates and views.

--- movielens/review/urls.py
+++ movielens/review/urls.py
@@ -3,5 +3,4 @@ from . import views     # to see the views.py file which is in same folder
 
 urlpatterns = [
     url(r'^movies/', views.top_20_movies),
-    url(r'^movies/(?P<MovieID>)\d+)$', views.show_movie), name = 'movie_detail'),
-    ]
+    url(r'^movies/(?P<movie_id>\d+)', views.movie_detail, name='movie_detail')]
--- movielens/review/views.py
+++ movielens/review/views.py
@@ -7,13 +7,19 @@ def top_20_movies(request):
     movies = Movie.objects.order_by(-average_rating)[:20]
     return render(request,
                   'review/movie.html',
+                  average_rating,
                   {'movies': movies})
 
 
-def movie_info(request, MovieID):
-    movies = Movie.objects.get(MovieID)
+def movie_detail(request, Movie_id):
+    movies = Movie.objects.get(pk=movie_id)
     m_ratings = movie.average_rating()
+    return render(request,
+                'review/movie.html',
+                    {'movies': movies})
 
 
-#
+def rater_detail(request, RaterID):
+    raters = Rater.objects.get(RaterID)
+
 # def Rater_info(request):
