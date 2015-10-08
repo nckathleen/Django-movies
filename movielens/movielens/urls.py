0558c52352e6eb35be2aa35811a9a4a5b7685b13
Kathleen Rauh <krauh2010@gmail.com>
2015-10-08 19:27:23 -0400

    Forgot a file.

--- movielens/movielens/urls.py
+++ movielens/movielens/urls.py
@@ -21,5 +21,6 @@ from users import views as user_views
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^login/$', user_views.user_login),
+    url(r'^register/$', user_views.user_register, name='user_register'),
     url(r'review/', include('review.urls')),
 ]
