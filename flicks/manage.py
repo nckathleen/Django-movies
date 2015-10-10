b4bac3793c11f5318dd57e63e4abcd92a7b19365
Kathleen Rauh <krauh2010@gmail.com>
2015-10-10 17:49:26 -0400

    Django startproject files 3

--- flicks/manage.py
+++ flicks/manage.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "moviedb.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "flicks.settings")
 
     from django.core.management import execute_from_command_line
 
--- moviedb/reviews/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from reviews.models import UserProfile
-
-# Register your models here.
-admin.site.register(Movie, MovieAdmin)
-admin.site.register(Rating, RatingAdmin)
-admin.sire.register(Rater)
