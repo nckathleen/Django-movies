612d5cf0d626c3425880829f059340184b99985c
Kathleen Rauh <krauh2010@gmail.com>
2015-10-10 21:55:41 -0400

    sql...

--- flicks/review/models.py
+++ flicks/review/models.py
@@ -8,12 +8,44 @@ from django.contrib.auth.models import User
 class Movie(models.Model):
     title = models.CharField(max_length=255)
 
+    def __str__(self):
+        return self.title
 
 class Rater(models.Model):
-    gender = models.CharField(max_length=1)
-    user = models.OneToOneField(null=True)
+
+    MALE = 'M'
+    FEMALE = 'F'
+    UNKNOWN = 'U'
+
+    GENDER_CHOICES = (
+        ('MALE', 'Male'),
+        ('FEMALE', 'Female'),
+        ('UNKNOWN', 'Unknown'))
+
+    gender = models.CharField(max_length=1, choices=GENDER_CHOICES)
+    # user = models.OneToOneField(null=True)
+
+    def __str__(self):
+        return self.id
 
 
-class Ratings(models.Model):
-    movie_id = models.ForeignKey(Movie)
-    rater_id = models.ForeignKey(Rater)
+class Rating(models.Model):
+    stars = models.PositiveSmallIntegerField()
+    movie = models.ForeignKey(Movie)
+    rating = models.ForeignKey(Rater)
+
+    def __str__(self):
+        return self.stars
+
+#
+# def load_ml_data():
+#
+#     import csv
+#     import json
+#
+#     # read movie data into fixtures
+#     movies = []
+#     with open (ml-m1/movies.dat, encoding='Windows-1252') as f:
+#         reader = csv.reader()
+#         for x in f:
+#             print()
