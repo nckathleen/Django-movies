546ff29d5ed9879970bd181d5d05b1b838cff036
Kathleen Rauh <krauh2010@gmail.com>
2015-10-14 17:43:15 -0400

    Aligning branch rewrite with master.

--- flicks/reviews/views.py
+++ flicks/reviews/views.py
@@ -1,18 +1,23 @@
 from django.shortcuts import render
+from django.db.models import Avg, Count
 from django.http import HttpResponse
 from .models import Rater, Movie, Rating
 
-
+# what the average of the stars are when we group them by movie
 def top_20_movies(request):
-    movie = Movie.objects.order_by(-average_rating)[:20]
+    # movie = Movie.objects.order_by(-average_rating)[:20]
+    popular_movies = Movie.objects.annotate(num_ratings=Count('rating')) \
+                                  .filter(num_ratings__gte=100)
+    movies = Movie.popular_movies.annotate(Avg('rating__stars')) \
+                          .order_by('-rating__stars__avg')[:20]
     return render(request,
-                  'review/movie.html',
-                  {'movie': movie})
+                  'review/top_movies.html',
+                  {'movies': movies})
 
 
 def movie_detail(request, movie_id):
     movie = Movie.objects.get(pk=movie_id)
-    return render(request
+    return render(request,
                   'reviews/movie_detail.html',
                   {'movie': movie})
 
