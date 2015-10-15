b973a6e2bb33ec81a3f8f8aa675b9941c1b5ec61
Kathleen Rauh <krauh2010@gmail.com>
2015-10-15 08:55:26 -0400

    *

--- flicks/reviews/urls.py
+++ flicks/reviews/urls.py
@@ -19,6 +19,6 @@ from . import views
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
-    url(r'movie/(?P<movie_id>\d+)$', views.movie_detail, name='movie_detail'),
-    url(r'rater/(?P<pk>\d+)$', views.rater_detail, name='rater_detail'),
+    url(r'^movie/(?P<movie_id>\d+)$', views.movie_detail, name='movie_detail'),
+    url(r'^rater/(?P<rater_id>\d+)$', views.rater_detail, name='rater_detail'),
 ]
--- flicks/reviews/views.py
+++ flicks/reviews/views.py
@@ -1,7 +1,50 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
 from django.db.models import Avg, Count
 from django.http import HttpResponse
 from .models import Rater, Movie, Rating
+from .forms import UserForm
+
+
+def user_login(request):
+    if request.method == "POST":    # attempting to log in
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(username=username, password=password)
+
+        if user is not None and user.is_active:
+            login(request, user)
+            return redirect('movie_detail')
+        else:
+            return render(request,
+                          'user/login.html',
+                          {'failed': True,
+                          'username':username})
+
+    return render(request,
+                  'reviews/login.html')
+
+
+def user_register(request):
+    if request.method == "POST":
+        form = UserForm(request.POST)
+
+        if form.is_valid():
+            user = form.save()
+            password = user.password
+            user.set_password(password)
+            user.save()
+
+            user = authenticate(username=user.username,
+                                password=password)
+            login(request, user)
+            return redirect('all_statuses')
+    else:
+        form = UserForm()
+    return render(request, 'reviews/register.html',
+                  {'form': form})
+
 
 # what the average of the stars are when we group them by movie
 def top_20_movies(request):
@@ -11,7 +54,7 @@ def top_20_movies(request):
     movies = Movie.popular_movies.annotate(Avg('rating__stars')) \
                           .order_by('-rating__stars__avg')[:20]
     return render(request,
-                  'review/top_movies.html',
+                  'reviews/top_movies.html',
                   {'movies': movies})
 
 
@@ -28,9 +71,9 @@ def rater_detail(request, rater_id):
     for rating in rater.rating_set.all():
         movie_ratings.append({
             'movie':rating.movie,
-            'ratings': '\u2605' * rating.ratings
+            'ratings': '\u2605' * rating.stars
         })
     return render (request,
-                   'review/rater_detail.html',
+                   'reviews/rater_detail.html',
                    {'rater':rater,
                    'movie_ratings': movie_ratings})
